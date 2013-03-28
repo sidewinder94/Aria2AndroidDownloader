7f0e76a143c1cecc9ef72b8dbdb7450941efb194
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2013-03-28 15:32:31 +0100

    Created Eclipse project

---
+++ gen/me/uni/antoinealizarrouk/aria2androiddownloader/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package me.uni.antoinealizarrouk.aria2androiddownloader;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
