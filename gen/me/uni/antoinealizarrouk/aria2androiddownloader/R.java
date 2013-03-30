2fd8baa7a792008058a67e3faa77b555288e0cc6
ZARROUK Antoine-Ali <antoineali.zarrouk@viacesi.fr>
2013-03-31 00:09:14 +0100

    Added french translation

--- gen/me/uni/antoinealizarrouk/aria2androiddownloader/R.java
+++ gen/me/uni/antoinealizarrouk/aria2androiddownloader/R.java
@@ -38,7 +38,6 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int exit=0x7f060002;
-        public static final int hello_world=0x7f060012;
         public static final int pref_connection_title=0x7f060003;
         public static final int pref_download_settings=0x7f060005;
         public static final int pref_summary_encryption=0x7f06000f;
