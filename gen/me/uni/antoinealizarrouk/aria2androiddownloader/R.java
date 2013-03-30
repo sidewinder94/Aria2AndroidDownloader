35579418c1f40e296b5191ef40b92103a77b614c
ZARROUK Antoine-Ali <antoineali.zarrouk@viacesi.fr>
2013-03-30 19:30:11 +0100

    Finished the connection part of the preferences
    Need a french translation in res/values-fr-rFR/strings.xml

--- gen/me/uni/antoinealizarrouk/aria2androiddownloader/R.java
+++ gen/me/uni/antoinealizarrouk/aria2androiddownloader/R.java
@@ -37,8 +37,23 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int exit=0x7f060003;
-        public static final int hello_world=0x7f060002;
+        public static final int exit=0x7f060002;
+        public static final int hello_world=0x7f060012;
+        public static final int pref_connection_title=0x7f060003;
+        public static final int pref_download_settings=0x7f060005;
+        public static final int pref_summary_encryption=0x7f06000f;
+        public static final int pref_summary_host=0x7f06000d;
+        public static final int pref_summary_password=0x7f060011;
+        public static final int pref_summary_port=0x7f06000e;
+        public static final int pref_summary_remote=0x7f06000c;
+        public static final int pref_summary_username=0x7f060010;
+        public static final int pref_title_encryption=0x7f060009;
+        public static final int pref_title_host=0x7f060007;
+        public static final int pref_title_password=0x7f06000b;
+        public static final int pref_title_port=0x7f060008;
+        public static final int pref_title_remote=0x7f060006;
+        public static final int pref_title_username=0x7f06000a;
+        public static final int title_settings_activity=0x7f060004;
     }
     public static final class style {
         /** 
