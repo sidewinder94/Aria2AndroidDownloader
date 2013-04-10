cfc9954f29b6a14cc7434e2ce23515ff1ec2c2e6
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2013-04-10 15:07:38 +0200

    Moved files and added the code for placing it in the right folder
    Moved aria2 executable file onto the raw directory
    Checking version from a version.txt file in the app directory
    Preparing the aria2 executable to be launched (placing it in the right
    directory on the device an make it executable)

--- gen/me/uni/antoinealizarrouk/aria2androiddownloader/R.java
+++ gen/me/uni/antoinealizarrouk/aria2androiddownloader/R.java
@@ -17,61 +17,65 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_exit=0x7f090001;
-        public static final int action_settings=0x7f090000;
+        public static final int action_exit=0x7f0a0001;
+        public static final int action_settings=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
+    }
+    public static final class raw {
+        public static final int aria2c=0x7f050000;
     }
     public static final class string {
-        public static final int FTP_specific_settings=0x7f060014;
-        public static final int HTTP_FTP_settings=0x7f060015;
-        public static final int HTTP_specific_settings=0x7f060016;
+        public static final int FTP_specific_settings=0x7f070015;
+        public static final int HTTP_FTP_settings=0x7f070016;
+        public static final int HTTP_specific_settings=0x7f070017;
         /**  Menu Strings 
  Menu Strings 
          */
-        public static final int action_settings=0x7f060001;
+        public static final int action_settings=0x7f070002;
         /**  Download Settings Menus
  Download Settings Menus
          */
-        public static final int advanced_settings=0x7f060012;
-        public static final int app_name=0x7f060000;
-        public static final int basic_settings=0x7f060013;
-        public static final int exit=0x7f060002;
-        public static final int metalink_specific_settings=0x7f060017;
-        public static final int pref_connection_title=0x7f060004;
-        public static final int pref_download_settings=0x7f060005;
-        public static final int pref_summary_encryption=0x7f06000f;
-        public static final int pref_summary_host=0x7f06000d;
-        public static final int pref_summary_password=0x7f060011;
-        public static final int pref_summary_port=0x7f06000e;
-        public static final int pref_summary_remote=0x7f06000c;
-        public static final int pref_summary_username=0x7f060010;
-        public static final int pref_title_encryption=0x7f060009;
-        public static final int pref_title_host=0x7f060007;
-        public static final int pref_title_password=0x7f06000b;
-        public static final int pref_title_port=0x7f060008;
+        public static final int advanced_settings=0x7f070013;
+        public static final int app_name=0x7f070001;
+        public static final int aria2_version=0x7f070000;
+        public static final int basic_settings=0x7f070014;
+        public static final int exit=0x7f070003;
+        public static final int metalink_specific_settings=0x7f070018;
+        public static final int pref_connection_title=0x7f070005;
+        public static final int pref_download_settings=0x7f070006;
+        public static final int pref_summary_encryption=0x7f070010;
+        public static final int pref_summary_host=0x7f07000e;
+        public static final int pref_summary_password=0x7f070012;
+        public static final int pref_summary_port=0x7f07000f;
+        public static final int pref_summary_remote=0x7f07000d;
+        public static final int pref_summary_username=0x7f070011;
+        public static final int pref_title_encryption=0x7f07000a;
+        public static final int pref_title_host=0x7f070008;
+        public static final int pref_title_password=0x7f07000c;
+        public static final int pref_title_port=0x7f070009;
         /**  Connection Settings 
  Connection Settings 
          */
-        public static final int pref_title_remote=0x7f060006;
-        public static final int pref_title_username=0x7f06000a;
+        public static final int pref_title_remote=0x7f070007;
+        public static final int pref_title_username=0x7f07000b;
         /**  Settings Activity 
  Settings Activity 
          */
-        public static final int title_settings_activity=0x7f060003;
-        public static final int torrent_metalink_settings=0x7f060018;
-        public static final int torrent_specific_settings=0x7f060019;
+        public static final int title_settings_activity=0x7f070004;
+        public static final int torrent_metalink_settings=0x7f070019;
+        public static final int torrent_specific_settings=0x7f07001a;
     }
     public static final class style {
         /** 
@@ -95,11 +99,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
--- src/me/uni/antoinealizarrouk/aria2androiddownloader/MainActivity.java
+++ src/me/uni/antoinealizarrouk/aria2androiddownloader/MainActivity.java
@@ -1,7 +1,14 @@
 package me.uni.antoinealizarrouk.aria2androiddownloader;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.*;
 import android.widget.Toast;
@@ -15,7 +22,50 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// TODO Import aria2 binairies into the project and launch it with the saved parameters
+		Context context = getApplicationContext();
+		//Trying to open the version file describing the aria2 installed version
+		try
+		{
+			String filePath = context.getFilesDir().getPath() + context.getPackageName();
+			String fileName = "version.txt";
+			File version = new File(filePath,fileName);
+			if (version.exists())
+			{
+				//Reading the file content if existing
+				FileInputStream fis = new FileInputStream(version);
+				byte[] buffer = new byte[fis.available()];
+				fis.read(buffer);
+				fis.close();
+				fis = null;
+				String versionString = new String(buffer);
+				if (versionString != getString(R.string.aria2_version));
+				{
+					/*If the version contained in the file is not equal to the one in the app 
+					 * copy the new version of aria2
+					 */
+					FileOutputStream fos = new FileOutputStream(version);
+					fos.write(getString(R.string.aria2_version).getBytes());
+					fos.close();
+					copyAria(fileName, filePath);
+				}
+				
+			}
+			else
+			{
+				version.createNewFile();
+				FileOutputStream fos = new FileOutputStream(version);
+				fos.write(getString(R.string.aria2_version).getBytes());
+				fos.close();
+				copyAria(fileName, filePath);
+			}
+		}
+		catch (IOException exception)
+		{
+			//TODO Handle Exception
+		}
+		//Aria2 is in the right folder ready to be executed
+		
+		// TODO Launch aria2 with the saved parameters
 		// http://stackoverflow.com/questions/5583487/hosting-an-executable-within-android-application 
 		// http://gimite.net/en/index.php?Run%20native%20executable%20in%20Android%20App
 	}
@@ -65,6 +115,29 @@ public class MainActivity extends Activity {
 	{
 		super.onDestroy();
 		// TODO shut down aria2
+	}
+	
+	private boolean copyAria(String fileName, String filePath)
+	{
+		try
+		{
+			Context context = getApplicationContext();
+			InputStream ins = context.getResources().openRawResource(R.raw.aria2c);
+			byte[] buffer = new byte[ins.available()];
+			ins.read(buffer);
+			ins.close();
+			ins = null;
+			FileOutputStream fos = context.openFileOutput(filePath + fileName, Context.MODE_PRIVATE);
+			fos.write(buffer);
+			fos.close();
+			Process process = Runtime.getRuntime().exec("/system/bin/chmod 744 " + filePath + fileName);
+			process.waitFor();
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
 	}
 	
 }
