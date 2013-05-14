3db329e06831a73f826535ab026ba155c25bf2c2
Maxence Winandy <maxence.winandy@gmail.com>
2013-05-14 12:53:04 +0200

    Fix rubbish files

--- gen/me/uni/antoinealizarrouk/aria2androiddownloader/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package me.uni.antoinealizarrouk.aria2androiddownloader;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
